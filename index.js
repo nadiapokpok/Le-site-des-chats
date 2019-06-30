3323faf43d3e3a25b3c552974b2f29c08a97bca4
nadia <nadia.aksouh@gmail.com>
2019-06-30 17:15:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+
+/*toggle dark theme*/
+
+function myFunction() {
+    var element = document.getElementById("body");
+    element.classList.toggle("mystyle");
+  } 
+
+
+  /*slide partie 6*/
+
+
+function shiftLeft() {
+    const boxes = document.querySelectorAll(".box");
+    const tmpNode = boxes[0];
+    boxes[0].className = "box move-out-from-left";
+
+    setTimeout(function() {
+        if (boxes.length > 5) {
+            tmpNode.classList.add("box--hide");
+            boxes[5].className = "box move-to-position5-from-left";
+        }
+        boxes[1].className = "box move-to-position1-from-left";
+        boxes[2].className = "box move-to-position2-from-left";
+        boxes[3].className = "box move-to-position3-from-left";
+        boxes[4].className = "box move-to-position4-from-left";
+        boxes[0].remove();
+
+        document.querySelector(".cards__container").appendChild(tmpNode);
+
+    }, 500);
+
+}
+
+function shiftRight() {
+    const boxes = document.querySelectorAll(".box");
+    boxes[4].className = "box move-out-from-right";
+    setTimeout(function() {
+        const noOfCards = boxes.length;
+        if (noOfCards > 4) {
+            boxes[4].className = "box box--hide";
+        }
+
+        const tmpNode = boxes[noOfCards - 1];
+        tmpNode.classList.remove("box--hide");
+        boxes[noOfCards - 1].remove();
+        let parentObj = document.querySelector(".cards__container");
+        parentObj.insertBefore(tmpNode, parentObj.firstChild);
+        tmpNode.className = "box move-to-position1-from-right";
+        boxes[0].className = "box move-to-position2-from-right";
+        boxes[1].className = "box move-to-position3-from-right";
+        boxes[2].className = "box move-to-position4-from-right";
+        boxes[3].className = "box move-to-position5-from-right";
+    }, 500);
+
+}
+
+/*animation partie2*/
+
+function myMove() {
+    var elem = document.getElementById("lasttry");   
+    var pos = 0;
+    var id = setInterval(frame, 0.5);
+    function frame() {
+      if (pos == 800) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = 800; 
+        elem.style.left = pos + 'px'; 
+      }
+    }
+  }
