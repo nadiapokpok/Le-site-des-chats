261369cf4b8a36abef5438827be80761155055e4
nadia <nadia.aksouh@gmail.com>
2019-07-30 14:23:16 +0200

    first commit

--- index.js
+++ index.js
@@ -1,73 +1,42 @@
+//FX NAVBAR
 
-/*toggle dark theme*/
+window.onscroll = function () { scrollFunction() };
 
-function myFunction() {
-    var element = document.getElementById("body");
-    element.classList.toggle("mystyle");
-  } 
-
-
-  /*slide partie 6*/
-
-
-function shiftLeft() {
-    const boxes = document.querySelectorAll(".box");
-    const tmpNode = boxes[0];
-    boxes[0].className = "box move-out-from-left";
-
-    setTimeout(function() {
-        if (boxes.length > 5) {
-            tmpNode.classList.add("box--hide");
-            boxes[5].className = "box move-to-position5-from-left";
-        }
-        boxes[1].className = "box move-to-position1-from-left";
-        boxes[2].className = "box move-to-position2-from-left";
-        boxes[3].className = "box move-to-position3-from-left";
-        boxes[4].className = "box move-to-position4-from-left";
-        boxes[0].remove();
-
-        document.querySelector(".cards__container").appendChild(tmpNode);
-
-    }, 500);
-
-}
-
-function shiftRight() {
-    const boxes = document.querySelectorAll(".box");
-    boxes[4].className = "box move-out-from-right";
-    setTimeout(function() {
-        const noOfCards = boxes.length;
-        if (noOfCards > 4) {
-            boxes[4].className = "box box--hide";
-        }
-
-        const tmpNode = boxes[noOfCards - 1];
-        tmpNode.classList.remove("box--hide");
-        boxes[noOfCards - 1].remove();
-        let parentObj = document.querySelector(".cards__container");
-        parentObj.insertBefore(tmpNode, parentObj.firstChild);
-        tmpNode.className = "box move-to-position1-from-right";
-        boxes[0].className = "box move-to-position2-from-right";
-        boxes[1].className = "box move-to-position3-from-right";
-        boxes[2].className = "box move-to-position4-from-right";
-        boxes[3].className = "box move-to-position5-from-right";
-    }, 500);
-
-}
-
-/*animation partie2*/
-
-function myMove() {
-    var elem = document.getElementById("lasttry");   
-    var pos = 0;
-    var id = setInterval(frame, 0.5);
-    function frame() {
-      if (pos == 800) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.top = 800; 
-        elem.style.left = pos + 'px'; 
-      }
+function scrollFunction() {
+    if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+        document.getElementById("menu").style.top = "23px";
+    } else {
+        document.getElementById("menu").style.top = "-55px";
     }
-  }
+} 
+
+//Fenetre Modale
+
+var part1 = document.getElementById('partie1');
+
+
+
+function show(choice){
+    var choix = document.getElementById(choice);
+    if (choix.style.display == 'none'){
+        choix.style.display = 'block'
+    }else{
+        choix.style.display = 'none';
+    }
+}
+
+function over(choiceOne){
+    console.log(choiceOne);
+    var monElt = document.getElementById(choiceOne);
+    console.log(monElt);
+    console.log(monElt.style)
+    monElt.style.backgroundColor = 'red';
+}
+
+function disparait(choiceTwo){
+    console.log(choiceTwo);
+    var monElt = document.getElementById(choiceTwo);
+    console.log(monElt);
+    console.log(monElt.style)
+    monElt.style.backgroundColor = '';
+}
